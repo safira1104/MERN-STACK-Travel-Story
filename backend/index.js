05e85a68588bb6c9ed2e8dd4a7657332ce3d511d
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-02-20 21:50:35 +0700

    Login api test

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,12 @@ const User = require("./models/user.model");
 mongoose.connect(config.connectionString);
 
 
-
 const app = express();
 
 app.use(express.json());
 app.use(cors({origin: "*"}));
 
-// TCreate Account
+// Create Account
 app.post("/create-account", async (req, res) => {
     const {fullName, email, password} = req.body;
 
@@ -61,6 +60,40 @@ app.post("/create-account", async (req, res) => {
         message: "Registration Successfull",
     });
             
+});
+
+// Login 
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and Password are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+    if(!isPasswordValid) {
+        return res.status(400).json({message: "Invalid Credentials"});
+    }
+
+    const accessToken = jwt.sign(
+        {userId: user._id},
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:"72h",
+        }
+    );
+
+    return res.json({
+        error: false,
+        message: "Login Successful",
+        user: { fullName: user.fullName, email: user.email},
+        accessToken,
+    });
 });
 
 app.listen(8000);
