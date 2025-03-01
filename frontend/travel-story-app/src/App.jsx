c1733c332caaea42975e93bc3ba1b528d05dacc7
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-03-01 22:20:42 +0700

    konfigurasi ulang react vite untuk menghubungkan tailwind

--- frontend/travel-story-app/src/App.jsx
+++ frontend/travel-story-app/src/App.jsx
@@ -1,23 +1,19 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import React from 'react'
-import Login from "./pages/Auth/login";
-import SignUp from "./pages/Auth/signup";
-import Home from "./pages/Home/home";
 
-
+import Login from "./pages/Auth/login"
+import SignUp from "./pages/Auth/signup"
+import Home from "./pages/Home/home"
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/dashboard" exact element={<Home />} />
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element= {<Home/>} />
         </Routes>
       </Router>
     </div>
-    
   )
 }
 
--- frontend/travel-story-app/tailwind.config.js
+++ frontend/travel-story-app/tailwind.config.js
@@ -1,18 +1,23 @@
+import PreviousMap_ from 'postcss/lib/previous-map';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      fontFamily: {
-        display: ["Poppins", "sans-serif"]
-      },
-      extend: {
-        // Colors used in the project
-        colors: {
-            primary: "#05B6D3",
-            secondary: "#EF863E",
-        },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      display: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      //Color used in the project
+      colors:{
+        primary: "#05B6D3",
+        secondary: "#EF863E",
       },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
+
