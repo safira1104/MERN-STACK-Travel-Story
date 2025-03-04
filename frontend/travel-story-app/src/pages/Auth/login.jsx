aaf36787edf36e2ca92b2816aceee8d520c04ebb
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-03-04 18:41:59 +0700

    menambahkan login API Call

--- frontend/travel-story-app/src/pages/Auth/login.jsx
+++ frontend/travel-story-app/src/pages/Auth/login.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom'
+import { useState  } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom'
 import PasswordInput from '../../components/Input/password-input';
+import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //Login API Call
+    try {
+       const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
+       });
+
+       // Handle successful login response
+       if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
+       }
+    } catch (error){
+      //Handle login error
+      if (
+        error.response &&
+        error.response.data && 
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred.   Please try again");
+      }
+    }
+  };
   return (
     <div className='h-screen bg-cyan-50 overflow-hidden relative'>
+
+      <div className='login-ui-box right-10 -top-40' />
+      <div className='login-ui-box bg-cyan-200 -bottom-40 right-1/2' />
+
       <div className='containe h-screen flex items-center justify-center px-20 mx-auto'>
          <div className='w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50'>
             <div>
@@ -18,15 +72,30 @@ const Login = () => {
          </div>
 
          <div className='w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20'>
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h4 className='text-2xl font-semibold mb-7'>Login</h4>
 
-            <input type="text" placeholder='Email' className='input-box' />
+            <input  
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={({ target }) => {
+                setEmail(target.value);
+              }}
+            />
 
-            <PasswordInput />
+            <PasswordInput 
+              value={password}
+              onChange={({ target }) => {
+                setPassword(target.value);
+              }}
+            />
+
+            {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
 
             <button type='submit' className='btn-primary'>
-              Login
+              LOGIN
             </button>
 
             <p className='text-xs text-slate-500 text-center my-4'>Or</p>
@@ -35,7 +104,7 @@ const Login = () => {
               type='submit' 
               className='btn-primary btn-light' 
               onClick={() => {
-                Navigate("/signUp");
+                navigate("/signUp");
               }}
             >
               CREATE ACCOUNT
