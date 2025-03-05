1fd3ca4b77b84076bb60117dc49f742f4ecea820
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-03-05 11:06:45 +0700

    // Define the Root component to handle the initial redirect

--- frontend/travel-story-app/src/App.jsx
+++ frontend/travel-story-app/src/App.jsx
@@ -10,7 +10,8 @@ const App = () => {
     <div>
       <Router>
         <Routes>
-          <Route path="/dashboard" exact element= {<Home/>} />
+          <Route path="/" exact element= {<Root />} />
+          <Route path="/dashboard" exact element= {<Home />} />
           <Route path="/login" exact element= {<Login />} />
           <Route path="/signup" exact element= {<SignUp />} />
         </Routes>
@@ -18,5 +19,18 @@ const App = () => {
     </div>
   )
 }
+
+// Define the Root component to handle the initial redirect
+const Root = () => {
+  //Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if authenticated, otherwise to login 
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+}; 
 
 export default App
