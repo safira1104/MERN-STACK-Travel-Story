7632d834cf6f316b9acee7bb8cf054a9b176a95d
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-03-07 09:31:02 +0700

    "Menambahkan komponen TravelStoryCard untuk menampilkan cerita perjalanan dengan gambar, judul, tanggal, deskripsi, dan lokasi."

--- frontend/travel-story-app/src/pages/Home/home.jsx
+++ frontend/travel-story-app/src/pages/Home/home.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar'
-import { useNavigate } from 'react-router-dom';
+import { data, useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
+import TravelStoryCard from '../../components/Cards/travel-story-card';
 
 const Home = () => {
 
   const navigate = useNavigate();
 
   const [userInfo, setUserInfo] = useState(null);
+  const [allStories, setAllStories] = useState([]);
 
   //Get User Info
   const getUserInfo = async () => {
@@ -26,16 +28,69 @@ const Home = () => {
     }
   };
 
-  useEffect(() => {
-    getUserInfo();
-    return () => {
+  // Get all travel stories
+  const getAllTravelStories = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-stories");
+      if (response.data && response.data.stories) {
+        setAllStories(response.data.stories);
+      }
+    } catch (error) {
+      console.log("An unexpected error occured. Please try again.")      
+    }
+  }
 
-    };
+  // Handle Edit Story Click
+  const handleEdit = (data) => {}
+
+  // Handle Travel Story Click
+  const handleViewStory = (data) => {}
+
+  // Handle Update Favourite
+  const updateIsFavourite = async (storyData) => {}
+
+  useEffect(() => {
+    getAllTravelStories();
+    getUserInfo();
+    return () => {};
   },[]);
 
   return (
     <>
      <Navbar userInfo={userInfo}/>
+
+     <div className='container mx-auto py-10'>
+      <div className='flex gap-7'>
+        <div className='flex-1'>
+          {
+            allStories.length > 0 ? (
+              <div className='grid grid-cols-2 gap-4'>
+                {allStories.map((item) => {
+                  return (
+                    <TravelStoryCard 
+                      key={item._id}
+                      imgUrl={item.imageUrl}
+                      title={item.title}
+                      story={item.story}
+                      date={item.visitedDate}
+                      visitedLocation={item.visitedLocation}
+                      isFavourite={item.isFavourite}
+                      onEdit={()=> handleEdit(item)}
+                      onClick={() => handleViewStory(item)}
+                      onFavouriteClick={() => updateIsFavourite(item)}
+
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <>Empty Card here</>
+            )}
+        </div>
+
+        <div className='w-[320px]'></div>
+      </div>
+     </div>
      
     </>
   );
